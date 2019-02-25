3a9986ef4681e3dbc47b06c985af9b96a3a7a922
tobiichiamane <tobiichiamane@outlook.com>
2019-02-25 15:26:05 +0800

    增加登录失败网页跳转

--- PixivUWP/LoadingPage.xaml.cs
+++ PixivUWP/LoadingPage.xaml.cs
@@ -39,6 +39,7 @@ using Windows.UI.Notifications;
 using Pixeez;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.System;
 
 //“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
 
@@ -200,6 +201,7 @@ namespace PixivUWP
 
         private async void status_Click(object sender, RoutedEventArgs e)
         {
+            await Launcher.LaunchUriAsync(new Uri(@"https://github.com/PixeezPlusProject/PixivUniversal/blob/developing/TroubleShoot.md"));
             await rollBackAnimation();
         }
 
